207ccd305f3acfe08d0f2e47492ad311da5f5cc1
Juan Velez <142271006+juanvelez008@users.noreply.github.com>
2023-12-19 20:57:29 +0100

    Proyecto completo colores rgb-dom

---
+++ script.js
@@ -0,0 +1,43 @@
+const inputRojo = document.getElementById(`rojo`);
+const inputVerde = document.getElementById(`verde`);
+const inputAzul = document.getElementById(`azul`);
+
+const textoRojo = document.getElementById(`texto-rojo`);
+const textoVerde = document.getElementById(`texto-verde`);
+const textoAzul = document.getElementById(`texto-azul`);
+
+//Tomar el valor del input
+let rojo = inputRojo.value;
+let verde = inputVerde.value;
+let azul = inputAzul.value;
+
+//Actualizar el texto de los parrafos
+textoRojo.innerText = rojo;
+textoVerde.innerText = verde;
+textoAzul.innerText = azul;
+
+function actualizarColor (rojo, verde, azul) {
+  const colorRGB = `rgb(${rojo}, ${verde}, ${azul})`;
+  document.body.style.backgroundColor = colorRGB;
+}
+
+// actualizar rojo
+inputRojo.addEventListener('change', (e) => {
+  rojo = e.target.value;
+  textoRojo.innerText = rojo;
+  actualizarColor (rojo,verde,azul)
+})
+
+// actualizar verde
+inputVerde.addEventListener('change', (e) => {
+  verde = e.target.value;
+  textoVerde.innerText = verde;
+  actualizarColor (rojo,verde,azul)
+})
+
+// actualizar azul
+inputAzul.addEventListener('change', (e) => {
+  azul = e.target.value;
+  textoAzul.innerText = azul;
+  actualizarColor (rojo,verde,azul)
+})
